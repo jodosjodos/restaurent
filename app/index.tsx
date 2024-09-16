650ec3539faf6980bde1dd88c717a451931e3a23
jodosjodos <jeandedieu2030@gmail.com>
2024-09-16 07:30:32 +0200

    [add] fix navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
 import DotComponent from "@/components/DotsComponents";
 import { images } from "@/constants";
-import { Link, Navigator } from "expo-router";
-import { useRef, useState } from "react";
+import { slides } from "@/constants/data";
+import { Link, useRouter } from "expo-router";
+import {useState } from "react";
 import {
   FlatList,
   Image,
@@ -23,45 +24,32 @@ const SimpleComponent = ({ item }: { item: SlideProp }) => {
   return (
     <View className="flex items-center justify-center gap-5">
       <Image source={item.image} />
-      <Text className="font-extrabold text-[#E4442A] text-4xl">{item.title}</Text>
+      <Text className="font-extrabold text-[#E4442A] text-4xl">
+        {item.title}
+      </Text>
       <Text className="text-2xl font-semibold">{item.description}</Text>
     </View>
   );
 };
 
 export default function Index() {
-  const slides = [
-    {
-      id: "1",
-      title: "Select a restaurant",
-      description: "Choose from a variety \n of restaurants around you.",
-      image: images.restaurant,
-    },
-    {
-      id: "2",
-      title: "Order food you like",
-      description: "Browse the menu and \norder your favorite meals.",
-      image: images.food,
-    },
-    {
-      id: "3",
-      title: "Deliver to your home",
-      description: "Enjoy fast delivery \nright to your door.",
-      image: images.delivery,
-    },
-  ];
   const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const router = useRouter()
   const nextItem = () => {
     if (currentIndex < slides.length - 1) {
       setCurrentIndex((prev) => prev + 1);
     } else {
-    
+      router.push("/sign-in");
     }
   };
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
       <SimpleComponent item={slides[currentIndex]} />
-      <DotComponent currentIndex={currentIndex} setCurrentIndex={setCurrentIndex} totalSlides={slides.length}/>
+      <DotComponent
+        currentIndex={currentIndex}
+        setCurrentIndex={setCurrentIndex}
+        totalSlides={slides.length}
+      />
       <TouchableOpacity
         onPress={nextItem}
         className="bg-[#FD8700] py-3 px-16  rounded-lg w-[95%] items-center justify-center mt-5"
---
+++ constants/data.ts
@@ -0,0 +1,22 @@
+import { images } from "@/constants/index";
+
+export const slides = [
+    {
+      id: "1",
+      title: "Select a restaurant",
+      description: "Choose from a variety \n of restaurants around you.",
+      image: images.restaurant,
+    },
+    {
+      id: "2",
+      title: "Order food you like",
+      description: "Browse the menu and \norder your favorite meals.",
+      image: images.food,
+    },
+    {
+      id: "3",
+      title: "Deliver to your home",
+      description: "Enjoy fast delivery \nright to your door.",
+      image: images.delivery,
+    },
+  ];
