8763f0a9292942016c76a92c6645cd30e648ce2a
jodosjodos <jeandedieu2030@gmail.com>
2024-09-16 05:22:23 +0200

    [init] init tailwind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex justify-center items-center bg-blue-500">
+      <Text className="text-red-500">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
